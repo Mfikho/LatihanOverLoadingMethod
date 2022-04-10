44b4ffb929287f81cf6147f726c647ad3553bcdd
mfikho <m.fikho@students.amikom.ac.id>
2022-04-10 21:14:53 +0700

    Praktikum 5

---
+++ Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Numbers
+    {
+        private int _i, _j;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _i = number1 < number3 ? number1 : (int)number3;
+            _j = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? _i : _j;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            _i = number1 > number3 ? number1 : (int)number3;
+            _j = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _i : _j;
+        }
+    }
+}
